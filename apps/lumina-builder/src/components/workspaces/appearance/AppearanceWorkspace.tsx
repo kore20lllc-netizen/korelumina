36cb3112c0f574983916ea0f472659c58ca7e2e4
kore20lllc-netizen <kore20lllc@gmail.com>
2026-07-09 20:03:18 -0400

    refactor(appearance): migrate workspace to composition engine

--- apps/lumina-builder/src/components/workspaces/appearance/AppearanceWorkspace.tsx
+++ apps/lumina-builder/src/components/workspaces/appearance/AppearanceWorkspace.tsx
@@ -1,18 +1,18 @@
+import {
+  LuminaWorkspaceComposer,
+} from "@/components/lumina";
+
 import { AppearancePreview } from "./parts/AppearancePreview";
 import { AppearanceSidebar } from "./parts/AppearanceSidebar";
 import { AppearanceToolbar } from "./parts/AppearanceToolbar";
 
 export function AppearanceWorkspace() {
   return (
-    <div className="flex h-full flex-col gap-6">
-      <AppearanceToolbar />
-
-      <div className="flex flex-1 gap-6">
-        <AppearanceSidebar />
-
-        <AppearancePreview />
-      </div>
-    </div>
+    <LuminaWorkspaceComposer
+      toolbar={<AppearanceToolbar />}
+      sidebar={<AppearanceSidebar />}
+      content={<AppearancePreview />}
+    />
   );
 }
 
