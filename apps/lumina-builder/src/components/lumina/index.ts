247dfc416ff5bf2f2955be33b274c7ca82f43198
kore20lllc-netizen <kore20lllc@gmail.com>
2026-07-12 15:56:44 -0400

    refactor(lumina): establish canonical design system export surface

--- apps/lumina-builder/src/components/lumina/index.ts
+++ apps/lumina-builder/src/components/lumina/index.ts
@@ -1,10 +1,16 @@
+export * from "./appearance";
+export * from "./background";
+export * from "./brand";
+export * from "./composition";
+export * from "./input";
+export * from "./inspector";
+export * from "./navigation";
+export * from "./surface";
+export * from "./telemetry";
+export * from "./theme";
+export * from "./workspace";
+
 export * from "./Blobs";
 export * from "./GlowCard";
 export * from "./LuminaButton";
 export * from "./LuminaSegmentedControl";
-
-export * from "./appearance";
-export * from "./input";
-export * from "./inspector";
-export * from "./surface";
-export * from "./composition";
