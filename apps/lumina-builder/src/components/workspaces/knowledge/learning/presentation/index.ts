9b81bf5b859ab203d5e823aad40debcfa725bf3a
kore20lllc-netizen <kore20lllc@gmail.com>
2026-08-08 07:34:30 -0400

    refactor(knowledge): remove obsolete local flagship card

--- apps/lumina-builder/src/components/workspaces/knowledge/learning/presentation/index.ts
+++ apps/lumina-builder/src/components/workspaces/knowledge/learning/presentation/index.ts
@@ -1,6 +1,5 @@
 export * from "./flagshipAppearance";
 export * from "./FlagshipPanel";
-export * from "./FlagshipCard";
 export * from "./FlagshipControl";
 export * from "./FlagshipTabs";
 export * from "./LearningStatusBadge";
