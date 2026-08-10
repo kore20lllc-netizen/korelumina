1d85662e6b6aade2e62aa7af4bb1adcd968e5db0
kore20lllc-netizen <kore20lllc@gmail.com>
2026-08-10 19:59:36 -0400

    refactor(knowledge): align canonical review supersession panel

--- apps/lumina-builder/src/components/workspaces/knowledge/production/governance/CanonicalReview.tsx
+++ apps/lumina-builder/src/components/workspaces/knowledge/production/governance/CanonicalReview.tsx
@@ -551,7 +551,11 @@ export function CanonicalReview({
             </div>
           </LuminaFlagshipPanel>
 
-          <LuminaStandardPremiumPanel className="h-full">
+          <LuminaFlagshipPanel
+            title={null}
+            className="h-full [&>div:nth-of-type(3)]:hidden"
+          >
+            <div className="p-5 sm:p-6">
             <div className="flex items-center gap-3">
               <ExecutivePremiumIcon
                 icon={GitCompareArrows}
@@ -594,7 +598,8 @@ export function CanonicalReview({
                 </div>
               </LuminaStandardPremiumCard>
             </div>
-          </LuminaStandardPremiumPanel>
+            </div>
+          </LuminaFlagshipPanel>
           </div>
         }
       />
