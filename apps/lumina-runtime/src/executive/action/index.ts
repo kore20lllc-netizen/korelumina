407eb1ddcad833ed018726e8ba850fb90454f1dd
kore20lllc-netizen <kore20lllc@gmail.com>
2026-08-12 15:26:33 -0400

    feat(executive): bind delegated owners to planned actions

--- apps/lumina-runtime/src/executive/action/index.ts
+++ apps/lumina-runtime/src/executive/action/index.ts
@@ -1,3 +1,4 @@
 export * from "./ExecutiveAction.js";
 export * from "./ExecutiveActionService.js";
 export * from "./ExecutiveDecisionActionProposalService.js";
+export * from "./ExecutiveDelegationActionProposalService.js";
